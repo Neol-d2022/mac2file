3cd972c6b8454dc4d29cb9c3635d1b4bfa8cf82f
neol-d2022 <neol_d2022@outlook.com>
2017-01-14 15:17:32 +0800

    Duplicate check, debugged

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <errno.h>
 
 #define MAX_RETRY_COUNT 128
+#define CHECKPOINT printf("CHECKPOINT\n"); fgets(buf, sizeof(buf), stdin);
 
 typedef struct {
     char *mac;
@@ -21,46 +22,76 @@ int cmpR(const void *a, const void *b) {
     else return 0;
 }
 
+int cmpI(const void *a, const void *b) {
+    RECORD **c = (RECORD**)a;
+    RECORD **d = (RECORD**)b;
+
+    return strcmp((*c)->mac, (*d)->mac);
+}
+
 int main (int argc, char** argv) {
-    RECORD r[32];
     char buf[32];
+    RECORD *r, **idxMac, *key, **resB;
     char *mac, *rssi, *p;
     size_t macLen, rssiLen;
     FILE *f;
-    unsigned int n = 0, i, c;
-    int res;
+    unsigned int n = 0, i, nr, c;
 
-    if(argc != 4) return 1;
+    if(argc != 5) return 1;
+    if(sscanf(argv[4], "%u", &nr) != 1) return 1;
+
+    c = 0;
+    r = (RECORD*)malloc(sizeof(*r) * nr);
+    idxMac = (RECORD**)malloc(sizeof(*idxMac) * nr);
     while(fgets(buf, sizeof(buf), stdin)) {
         if((p = strchr(buf, '\n'))) *p = '\0';
         if((p = strchr(buf, '\r'))) *p = '\0';
 
         macLen = strcspn(buf, "\t");
         rssiLen = strlen(buf + macLen + 1);
-        i = n;
-        if(i >= 32) {
-            free(r[31].mac);
-            free(r[31].rssi);
-            i = 31;
-        }
+
         mac = (char*)malloc(macLen + 1);
         memcpy(mac, buf, macLen); mac[macLen] = '\0';
         rssi = (char*)malloc(rssiLen + 1);
         memcpy(rssi, buf + macLen + 1, rssiLen); rssi[rssiLen] = '\0';
-        r[i].mac = mac;
-        r[i].rssi = rssi;
-        r[i].timestamp = time(0);
-        n = i + 1;
-        qsort(r, n, sizeof(r[0]), cmpR);
 
+        for(i = 0; i < n; i += 1) idxMac[i] = r + i;
+        qsort(idxMac, n, sizeof(idxMac[0]), cmpI);
+        key = malloc(sizeof(*key));
+        key->mac = mac;
+        
+        resB = (RECORD**)bsearch(&key, idxMac, n, sizeof(idxMac[0]), cmpI);
+        free(key);
+
+        if(resB) {
+            free((*resB)->rssi);
+            (*resB)->rssi = rssi;
+            free(mac);
+
+            (*resB)->timestamp = time(0);
+        }
+        else {
+            i = n;
+            if(i >= nr) {
+                free(r[nr - 1].mac);
+                free(r[nr - 1].rssi);
+                i = nr - 1;
+            }
+            r[i].mac = mac;
+            r[i].rssi = rssi;
+            r[i].timestamp = time(0);
+            n = i + 1;
+        }
+        
+        qsort(r, n, sizeof(r[0]), cmpR);
         f = fopen(argv[2], "w");
         if(!f) return -2;
         for(i = 0; i < n; i += 1)
             fprintf(f, "%s\t%s\t%u\n", r[i].mac, r[i].rssi, (unsigned int)(r[i].timestamp));
         fclose(f);
 
-        c = 0; res = ~0;
-        while(res) { if(c >= MAX_RETRY_COUNT) return -2; res = rename(argv[1], argv[3]); c += 1; if(errno == ENOENT && res) break; }
+        if(rename(argv[1], argv[3])) { c += 1; if(c >= MAX_RETRY_COUNT) return -2; }
+        else if(c) c = 0;
 
         if(rename(argv[2], argv[1])) return -2;
         remove(argv[3]);
