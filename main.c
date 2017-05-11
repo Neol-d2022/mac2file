bb1a7c2d35b3343269dbb51a24df4fa14ac18e4b
Neol-d2022 <neol_d2022@outlook.com>
2017-05-12 01:00:47 +0800

    OUI

--- main.c
+++ main.c
@@ -9,10 +9,13 @@
     printf("CHECKPOINT\n"); \
     fgets(buf, sizeof(buf), stdin);
 
+char *UNKNOWN_ORGNAME = "UNKNOWN MANUFACTURER";
+
 typedef struct
 {
     char *mac;
     char *rssi;
+    char *orgname;
     time_t timestamp;
     unsigned int channel;
 } RECORD;
@@ -45,18 +48,65 @@ unsigned int freq2ch(unsigned int freq)
     return ((freq - 2412) / 5) + 1;
 }
 
+typedef struct
+{
+    char asgmt[16];
+    char *orgname;
+} oui_t;
+
+void loadOui(FILE *oui, oui_t **oui_base, unsigned int *ouiLen)
+{
+    char buf[1024], *chr;
+    unsigned int c = 0, i = 0;
+
+    while (fgets(buf, sizeof(buf), oui))
+        c += 1;
+    rewind(oui);
+
+    *ouiLen = c;
+    *oui_base = (oui_t *)malloc(sizeof(**oui_base) * c);
+
+    while (fgets(buf, sizeof(buf), oui))
+    {
+        if ((chr = strchr(buf, '\n')))
+            *chr = '\0';
+        if ((chr = strchr(buf, '\r')))
+            *chr = '\0';
+        if (!(chr = strchr(buf, ',')))
+            continue;
+        *chr = '\0';
+        memcpy(((*oui_base)[i]).asgmt, buf, (size_t)chr - (size_t)buf);
+        ((*oui_base)[i]).orgname = strdup(chr + 1);
+        i += 1;
+    }
+}
+
+int cmpOui(const void *a, const void *b)
+{
+    oui_t *c, *d;
+
+    c = (oui_t *)a;
+    d = (oui_t *)b;
+
+    return strcmp(c->asgmt, d->asgmt);
+}
+
 int main(int argc, char **argv)
 {
     char buf[32], channel[8];
+    oui_t outKey;
     RECORD *r, **idxMac, *key, **resB;
-    char *mac, *rssi, *p;
+    char *mac, *rssi, *p, *orgname;
     size_t macLen, rssiLen, channelLen;
-    FILE *f;
+    FILE *f, *oui;
+    oui_t *oui_base, *org;
     time_t ts;
-    unsigned int n = 0, i, nr, c, freq;
+    unsigned int n = 0, i, nr, c, freq, ouiLen;
     int timeout, rssiA, rssiB;
 
-    if (argc != 7)
+    if (argc != 8)
+        return 1;
+    if (!(oui = fopen(argv[7], "r")))
         return 1;
     if (sscanf(argv[4], "%u", &nr) != 1)
         return 1;
@@ -66,6 +116,9 @@ int main(int argc, char **argv)
     c = 0;
     r = (RECORD *)malloc(sizeof(*r) * nr);
     idxMac = (RECORD **)malloc(sizeof(*idxMac) * nr);
+    loadOui(oui, &oui_base, &ouiLen);
+    fclose(oui);
+    qsort(oui_base, ouiLen, sizeof(*oui_base), cmpOui);
     while (fgets(buf, sizeof(buf), stdin))
     {
         if ((p = strchr(buf, '\n')))
@@ -100,13 +153,15 @@ int main(int argc, char **argv)
         {
             sscanf((*resB)->rssi, "%d", &rssiA);
             sscanf(rssi, "%d", &rssiB);
-            if((rssiB >= rssiA && (*resB)->timestamp == ts) || ts > (*resB)->timestamp) {
+            if ((rssiB >= rssiA && (*resB)->timestamp == ts) || ts > (*resB)->timestamp)
+            {
                 free((*resB)->rssi);
                 (*resB)->rssi = rssi;
                 sscanf(channel, "%u", &freq);
                 (*resB)->channel = freq2ch(freq);
             }
-            else free(rssi);
+            else
+                free(rssi);
             free(mac);
 
             (*resB)->timestamp = ts;
@@ -125,6 +180,14 @@ int main(int argc, char **argv)
             r[i].timestamp = ts;
             sscanf(channel, "%u", &freq);
             r[i].channel = freq2ch(freq);
+            memcpy(outKey.asgmt, mac, 8);
+            outKey.asgmt[8] = '\0';
+            org = (oui_t *)bsearch(&outKey, oui_base, ouiLen, sizeof(*oui_base), cmpOui);
+            if (org)
+                orgname = org->orgname;
+            else
+                orgname = UNKNOWN_ORGNAME;
+            r[i].orgname = orgname;
             n = i + 1;
         }
 
@@ -145,7 +208,7 @@ int main(int argc, char **argv)
         if (!f)
             return -2;
         for (i = 0; i < n; i += 1)
-            fprintf(f, "%s\t%s\t%u\t%u\t%s\n", r[i].mac, r[i].rssi, (unsigned int)(r[i].timestamp), r[i].channel, argv[6]);
+            fprintf(f, "%s\t%s\t%u\t%u\t%s\t%s\n", r[i].mac, r[i].rssi, (unsigned int)(r[i].timestamp), r[i].channel, argv[6], r[i].orgname);
         fclose(f);
 
         remove(argv[3]);
